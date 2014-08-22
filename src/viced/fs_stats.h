2323ae075ab167025cde51c5e9a694fa8e093bcf
Michael Meffie <mmeffie@sinenomine.net>
2014-08-22 16:52:48 -0400

    ipv6: wip convert FS_HostAddr to rx_address
    Change-Id: I91fde5d66526660830401be3cda87b6b935e264a

--- src/viced/fs_stats.h
+++ src/viced/fs_stats.h
@@ -325,12 +325,10 @@ extern struct fs_stats_FullPerfStats afs_FullPerfStats;
 extern struct afs_PerfStats afs_perfstats;
 
 /*
-  * FileServer's name and IP address, both network byte order and
-  * host byte order.
+  * FileServer's name and IP address.
   */
 extern char FS_HostName[];
-extern struct rx_sockaddr FS_HostAddr_NBO;
-extern struct rx_address FS_HostAddr_HBO;
+extern struct rx_address FS_HostAddr;
 
 /* Logging helper functions */
 struct fsstats {

e3b42664efce2489aa8309e02013b4ce2bc0b0cf
Marcio <mbarbosa@sinenomine.net>
2014-08-22 08:03:28 -0700

    spaces: formatting

--- src/auth/netrestrict.c
+++ src/auth/netrestrict.c
@@ -436,13 +436,13 @@ afsconf_ParseNetFiles(afs_uint32 addrbuf[], afs_uint32 maskbuf[],
     addrbuf_ipv4 = (struct rx_sockaddr *)calloc(max, sizeof(struct rx_sockaddr));
 
     if (maskbuf)
-    	maskbuf_ipv4 = (struct rx_sockaddr *)calloc(max, sizeof(struct rx_sockaddr));
+	maskbuf_ipv4 = (struct rx_sockaddr *)calloc(max, sizeof(struct rx_sockaddr));
 
-    for(i = 0; i < max; i++) {
-    	rx_ipv4_to_sockaddr(addrbuf[i], 0, 0, &addrbuf_ipv4[i]);
+    for (i = 0; i < max; i++) {
+	rx_ipv4_to_sockaddr(addrbuf[i], 0, 0, &addrbuf_ipv4[i]);
 
-    	if (maskbuf)
-    		rx_ipv4_to_sockaddr(maskbuf[i], 0, 0, &maskbuf_ipv4[i]);
+	if (maskbuf)
+	    rx_ipv4_to_sockaddr(maskbuf[i], 0, 0, &maskbuf_ipv4[i]);
     }
 
     code = afsconf_ParseNetFiles2(addrbuf_ipv4, maskbuf_ipv4, mtubuf, max, reason, niFileName, nrFileName);

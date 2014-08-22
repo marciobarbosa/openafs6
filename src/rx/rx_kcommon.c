84d423551caf5fce8b0a36002f3497de12e016b5
Marcio <mbarbosa@sinenomine.net>
2014-08-22 07:25:53 -0700

    bug fixed

--- src/rx/rx_kcommon.c
+++ src/rx/rx_kcommon.c
@@ -372,7 +372,7 @@ rxi_InitPeerParams(struct rx_peer *pp)
     afs_int32 i;
     afs_int32 mtu;
 
-    i = rxi_Findcbi(&pp->saddr.rxsa_in_addr);
+    i = rxi_Findcbi(pp->saddr.rxsa_in_addr);
     if (i == -1) {
 	rx_rto_setPeerTimeoutSecs(pp, 3);
 	pp->ifMTU = MIN(RX_REMOTE_PACKET_SIZE, rx_MyMaxSendSize);
